422fee649e52a568b9a9d325abf498f50161605b
Paru - Paulo Victor Pinheiro <99094718+Paru369@users.noreply.github.com>
2023-07-17 10:25:21 -0300

    [LOR-51] Componente - PrimaryButton e SecondButton (#6)
    * criacao-dos-botoes
    
    * Excusão do ContentView que não foi alterado

---
+++ loryblu/loryblu/Preview Content/ContentButtons.swift
@@ -0,0 +1,48 @@
+//
+//  ContentButtons.swift
+//  LoryBlu
+//
+//  Created by Paulo Pinheiro on 7/16/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct Buttons: View{
+    init(title: String, style: ButtonStyle = .primary, action: @escaping () -> Void){
+        self.title = title
+        self.style = style
+        self.action =  action
+    }
+   
+    enum ButtonStyle{
+        case primary
+        case second
+    }
+    var title: String
+    var style: ButtonStyle
+    let action: () -> Void
+    var body: some View{
+        Button(title){
+           action()
+        }
+            .textCase(.uppercase)
+            .font(Font.custom("Inter", size: 16) .weight(.bold))
+            .multilineTextAlignment(.center)
+            .foregroundColor( ((style == .primary) ? .white : Style.ColorPalette.buttonPrimary))
+            .frame(maxWidth: .infinity, maxHeight: 44, alignment: .center)
+            .background((style == .primary) ? Style.ColorPalette.buttonPrimary : .white)
+            .cornerRadius(10)
+            .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .inset(by:-1)
+                .stroke((style == .primary) ? .white :  Style.ColorPalette.buttonPrimary))
+        
+    }
+            
+        }
+    
+
+
+
