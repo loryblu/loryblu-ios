318994fcb4036aaad3f90c65f984a6317c9d022f
Suelen Cristina Vaz <100352837+sucriss2@users.noreply.github.com>
2023-07-22 14:08:52 -0300

    [LOR-68] Create components IconLabel and PasswordRulesView (#9)
    * [LOR-68] Initial creation the Components IconLabel
    
    * [LOR-68] Setting icon with label change color and image the icon.
    
    * [LOR-68] Create LocalizableString with some texts.
    - Create components PasswordRulesView with IconLabel finished.
    
    * [LOR-68] FIX: Remove unnecessary file

---
+++ loryblu/loryblu/Style/LBStrings.swift
@@ -0,0 +1,32 @@
+//
+//  LBLocalized.swift
+//  LoryBlu
+//
+//  Created by Suh on 21/07/23.
+//
+
+import Foundation
+import SwiftUI
+
+enum LBStrings {
+
+}
+
+extension LBStrings {
+    enum General {
+        static let next = LocalizedStringKey("next")
+        static let send = LocalizedStringKey("send")
+        static let hello = LocalizedStringKey("hello")
+    }
+}
+
+extension LBStrings {
+    enum Rules {
+        static let title = String(localized: "rule_title")
+        static let rule1 = String(localized: "rule1")
+        static let rule2 = String(localized: "rule2")
+        static let rule3 = String(localized: "rule3")
+        static let rule4 = String(localized: "rule4")
+        static let rule5 = String(localized: "rule5")
+    }
+}
