a89315fd932e0b3eea1bcb3a6d1f6a9e8dd32d0c
parutech <paruvitu@gmail.com>
2023-07-25 20:17:11 -0300

    final

---
+++ loryblu/loryblu/Views/LoginView/ExternalLogin.swift
@@ -0,0 +1,20 @@
+//
+//  ExternalLogin.swift
+//  LoryBlu
+//
+//  Created by Paulo Pinheiro on 7/25/23.
+//
+
+import SwiftUI
+
+struct ExternalLogin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExternalLogin_Previews: PreviewProvider {
+    static var previews: some View {
+        ExternalLogin()
+    }
+}
---
+++ loryblu/loryblu/Views/LoginView/LabelledDivider.swift
@@ -0,0 +1,20 @@
+//
+//  LabelledDivider.swift
+//  LoryBlu
+//
+//  Created by Paulo Pinheiro on 7/25/23.
+//
+
+import SwiftUI
+
+struct LabelledDivider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LabelledDivider_Previews: PreviewProvider {
+    static var previews: some View {
+        LabelledDivider()
+    }
+}
