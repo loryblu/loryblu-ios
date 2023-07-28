8d4a32e529e9ec88c484b88d8d36e5cd7fb5d30a
parutech <paruvitu@gmail.com>
2023-07-28 07:41:12 -0300

    validation

--- loryblu/loryblu/Views/LoginView/LoginView.swift
+++ loryblu/loryblu/Views/LoginView/LoginView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct LoginView: View {
     
+    let emailPredicate = NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}" )
+    
+    let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", "ˆ(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z].{8,}$")
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    
     @State private var rememberMe = false
+    
+    
     
     func sendLogin(){
         //your logic here
@@ -25,26 +34,33 @@ struct LoginView: View {
                 CustomTextField(
                     style: .common,
                     icon: Icon.mail,
-                    title: "User",
-                    text: .constant("Email"),
+                    title: "Email",
+                    text:$email,
                     isHidden: .constant(true),
-                    textFiledState: .active).padding(.bottom, 16)
+                    textFiledState: .active)
                 
                 CustomTextField(
                     style: .password,
                     icon: Icon.lock,
-                    title: "User",
-                    text: .constant("Senha"),
+                    title: "Senha",
+                    text:$password,
                     isHidden: .constant(false),
                     textFiledState: .active)
+                
             }
-            .padding(.horizontal, 24)
             
-            HStack{
-                Toggle("Agree", isOn: $rememberMe).padding(.trailing, 18.0).labelsHidden()
-                Text("Lembrar").font(Style.Typography.subtitle).multilineTextAlignment(.trailing).padding(.trailing, 24)
+            VStack{
+                if emailPredicate.evaluate(with: email) && !email.isEmpty {
+                    Text("Email inválido")}
+                
+                HStack{
+                    
+                    Toggle("Agree", isOn: $rememberMe).padding(.trailing, 18.0).labelsHidden()
+                    Text("Lembrar").font(Style.Typography.subtitle).multilineTextAlignment(.trailing)
+                }
             }
-            .frame(maxWidth: .infinity, alignment: .trailing).padding(.top,13)
+            .frame(maxWidth: .infinity, maxHeight: 77, alignment: .trailing)
+            
             
             Buttons(title: "entrar", action: sendLogin).padding(.horizontal, 24)
                 .padding(.top, 51)
@@ -61,7 +77,7 @@ struct LoginView: View {
                 )
             }
             .frame(maxWidth: .infinity, alignment: .trailing)
-            .padding(.trailing, 24)
+            
             
             ExternalLogin()
                 .padding(.top, 53.0)
@@ -73,6 +89,7 @@ struct LoginView: View {
                 Button("Registrar agora"){}.font(Style.Typography.caption).foregroundColor(Style.ColorPalette.buttonPrimary).multilineTextAlignment(.trailing)
             }.padding(.top, 32.0)
         }
+        .padding(24)
         
     }
 }
