fd3971cef8dc9bf9d0cd69857f5c3e50d7d706c1
Suelen Critina Vaz <sucris12@gmail.com>
2023-11-03 23:30:38 -0300

    Setting layout screen register tasks.

---
+++ loryblu/loryblu/Feature/Home/Locbook/LocbookRegisterView.swift
@@ -0,0 +1,36 @@
+//
+//  LocbookRegisterView.swift
+//  LoryBlu
+//
+//  Created by Suh on 03/11/23.
+//
+
+import SwiftUI
+
+struct LocbookRegisterView: View {
+    var body: some View {
+        VStack(alignment: .center){
+
+            Text(LBStrings.Locbook.title)
+                .font(LBFont.titleAction)
+                .foregroundStyle(LBColor.borderSelectLogbook)
+
+            LBIcon.dailyList.image
+                .resizable()
+                .scaledToFit()
+                .padding(.top, 70)
+
+            LBButton(
+                title: LBStrings.Locbook.button,
+                style: .primaryActivated) {
+                    //
+                }
+                .padding(.top, 82)
+        }
+        .padding(.init(top: 15, leading: 26, bottom: 50, trailing: 26))
+    }
+}
+
+#Preview {
+    LocbookRegisterView()
+}
