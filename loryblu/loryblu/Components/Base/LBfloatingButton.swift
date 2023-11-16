53f03ba465ff19d06288ee623e566be68982535d
Rodrigo <rodrigoeduardosilv@gmail.com>
2023-11-15 21:28:28 -0300

    add floating button

---
+++ loryblu/loryblu/Components/Base/LBfloatingButton.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct LBfloatingButton: View {
+    var text: String
+    var icon: LBIcon?
+    var action: () -> Void
+    var body: some View {
+        Button(action: action) {
+            HStack(alignment: .center) {
+                if let icon = icon {
+                    icon.image
+                        .frame(width: 24)
+                        .padding(.leading ,8)
+                }
+                Text(text)
+                    .font(LBFont.button)
+                    .frame(height: 48)
+                    .frame(minWidth: 155,alignment: .center)
+            }
+            .background(LBColor.backgroundImagetrails)
+            .foregroundColor(LBColor.background)
+            .cornerRadius(48)
+            .shadow(color: .black.opacity(0.3) ,radius: 7 ,x: 3, y: 10)
+        }
+        .frame(height: 48)
+    }
+}
+
+#Preview {
+    VStack {
+        LBfloatingButton(text: "NOVA TAREFA", icon: LBIcon.plus) {
+            print("Ok")
+        }
+    }
+}
