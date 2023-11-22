84553d94adaec615f21b9a87233f6e2d7161759c
Suelen Critina Vaz <sucris12@gmail.com>
2023-11-21 21:03:58 -0300

    Chage call App

--- loryblu/loryblu/LorybluApp.swift
+++ loryblu/loryblu/LorybluApp.swift
@@ -2,23 +2,15 @@ import SwiftUI
 
 @main
 struct LorybluApp: App {
-//    @StateObject private var appData: AppData = .init()
+    @StateObject private var appData: AppData = .init()
     var body: some Scene {
         WindowGroup {
-            FrequencyRotine(sunday: true,
-                            monday: false,
-                            tuesday: true,
-                            wednesday: false,
-                            thurday: true,
-                            friday: false,
-                            satuday: true, title: .constant(LBStrings.Locbook.titleStudy
-        ))
-//            AppEntryPoint()
-//                .environmentObject(appData)
-//                .onOpenURL { url in
-//                    let deepLinkHandler = DeepLinkHandler()
-//                    deepLinkHandler.handleDeepLink(with: url, appData: appData)
-//                }
+            AppEntryPoint()
+                .environmentObject(appData)
+                .onOpenURL { url in
+                    let deepLinkHandler = DeepLinkHandler()
+                    deepLinkHandler.handleDeepLink(with: url, appData: appData)
+                }
         }
     }
 }
