a17431f87fc8b2e4a6c38ed2dc1561eddd26ca90
Rodrigo <rodrigoeduardosilv@gmail.com>
2023-11-22 20:51:20 -0300

    add overlay in datepicker

--- loryblu/loryblu/Components/Base/LBTextField/LBDatePicker.swift
+++ loryblu/loryblu/Components/Base/LBTextField/LBDatePicker.swift
@@ -3,28 +3,31 @@ import SwiftUI
 struct LBDatePicker: View {
     let onConfirm: (Date) -> Void
     let onCancel: () -> Void
-    @Environment(\.dismiss) var dismiss
     @State var selectedDate = Date()
     var body: some View {
 
         ZStack(alignment: .center ) {
-            VStack {
-                DatePicker(LBStrings.Register.birthDay, selection: $selectedDate, displayedComponents: .date)
-                                .datePickerStyle(.graphical)
-                                .padding()
-                                .background(Color.white.cornerRadius(20))
-                                .clipped()
-                                .environment(\.locale, Locale.init(identifier: "pt"))
-                                .foregroundColor(LBColor.background)
-                            .padding(20)
-                HStack {
-                    Button(LBStrings.General.confirm) {
-                        onConfirm(selectedDate)
-                    }
-                    Button(LBStrings.General.cancel) {
-                        onCancel()
-                    }
-                }
+            Color.gray.opacity(0.2)
+                 .ignoresSafeArea()
+                 .overlay {
+                     VStack {
+                         DatePicker(LBStrings.Register.birthDay, selection: $selectedDate, displayedComponents: .date)
+                             .datePickerStyle(.graphical)
+                             .padding()
+                             .background(Color.white.cornerRadius(20))
+                             .clipped()
+                             .environment(\.locale, Locale.init(identifier: "pt"))
+                             .foregroundColor(LBColor.background)
+                         HStack {
+                             Button(LBStrings.General.confirm) {
+                                 onConfirm(selectedDate)
+                             }
+                             Button(LBStrings.General.cancel) {
+                                 onCancel()
+                             }
+                         }.padding(.vertical,-30)
+                     }
+                     .padding(20)
             }
         }
     }
