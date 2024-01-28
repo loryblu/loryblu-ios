1f53e02d9d3a76875cf16c21cd60a84af034d185
Suelen Critina Vaz <sucris12@gmail.com>
2024-01-28 09:01:41 -0300

    Add appIcon, and code firebase

---
+++ loryblu/loryblu/FirebaseHelper.swift
@@ -0,0 +1,11 @@
+import UIKit
+import FirebaseCore
+
+class FirebaseHelper: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+      FirebaseApp.configure()
+
+    return true
+  }
+}
--- loryblu/loryblu/LorybluApp.swift
+++ loryblu/loryblu/LorybluApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct LorybluApp: App {
+    @UIApplicationDelegateAdaptor(FirebaseHelper.self) var delegate
+    
     @StateObject private var appData: AppData = .init()
     var body: some Scene {
         WindowGroup {
