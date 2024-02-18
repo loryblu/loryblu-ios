c4b91d920a209f3aa84fc732319a50fa3d0c1d76
Rodrigo <rodrigocavalcante.pro@gmail.com>
2024-02-18 19:53:56 -0300

    [LOR-225] - update model files

--- loryblu/loryblu/Feature/Entities/TaskModel.swift
+++ loryblu/loryblu/Feature/Entities/TaskModel.swift
@@ -1,13 +1,7 @@
-//
-//  TaskModel.swift
-//  LoryBlu
-//
-//  Created by Rodrigo Cavalcante on 15/02/24.
-//
 import Foundation
 
-struct TaskModel {
-    var uuid: UUID = UUID()
-    var actionType: String
-    var locbookTask: LocbookTask
+ struct TaskModel {
+     var uuid: UUID = UUID()
+     var actionType: String
+     var locbookTask: LocbookTask
 }
--- loryblu/loryblu/Feature/Entities/TaskNetworkModels.swift
+++ loryblu/loryblu/Feature/Entities/TaskNetworkModels.swift
@@ -1,21 +1,14 @@
-//
-//  NetworkModels.swift
-//  LoryBlu
-//
-//  Created by Rodrigo Cavalcante on 15/02/24.
-//
-
 struct NetworkDataModel : Decodable {
-    var study: [TasksNetworkModel]
-    var routine: [TasksNetworkModel]
-}
+     var study: [TasksNetworkModel]
+     var routine: [TasksNetworkModel]
+ }
 
-struct TasksNetworkModel : Decodable {
-    var childrenId: Int
-    var shift: String
-    var frequency: [String]
-    var order: Int
-    var categoryId: String
-    var categoryTitle: String
-    var updatedAt: String
-}
+ struct TasksNetworkModel : Decodable {
+     var childrenId: Int
+     var shift: String
+     var frequency: [String]
+     var order: Int
+     var categoryId: String
+     var categoryTitle: String
+     var updatedAt: String
+ }
