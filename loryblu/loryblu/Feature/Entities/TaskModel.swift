14d9f885029067b01cb9e6db8f759ad20fdfa488
Rodrigo <72306040+rodrigoliveirac@users.noreply.github.com>
2024-02-19 14:22:48 -0300

    LOR-225 - Get all tasks from api. (#76)
    * [LOR-225] - Creating data models
    
    * [LOR-225] - Fetching all tasks from api by TaskRepository
    
    * [LOR-225] - Creating ViewModel for TasksView in order to fetch tasks from TaskRepository
    
    * [LOR-225] - update model files
    
    * [LOR-225] - Update models
    
    * [LOR-225] - Display tasks
    
    * [LOR-225] -  Switch RegisterView to ListTasksView then refactor NavStack.
    
    * [LOR-225] - fix
    
    * [LOR-225] - improve code design
    
    * [LOR-225] - improve code design
    
    * [LOR-225] - Removing unnecessary lines

---
+++ loryblu/loryblu/Feature/Entities/TaskModel.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+ struct TaskModel {
+     var uuid: UUID = UUID()
+     var actionType: String
+     var locbookTask: LocbookTask
+     var image: String
+}
--- loryblu/loryblu/Repositories/TaskRepository.swift
+++ loryblu/loryblu/Repositories/TaskRepository.swift
@@ -1,30 +1,28 @@
 import Foundation
 import Factory
-
 class TaskRepository {
-
     let network: Network
-
+    
     init(network: Network) {
         self.network = network
     }
-
+    
     var token = Container.shared.appData
-
+    
     func taskRegister(with locBookTask: LocbookTask, token: String, childrenID: Int) async -> Bool {
-
+        
         guard let categoryId = locBookTask.categoryId?.description else {
             return false
         }
-
+        
         guard let shift = locBookTask.shift else {
             return false
         }
-
+        
         guard let frequency = locBookTask.frequency else {
             return false
         }
-
+        
         let request = RequestModel.Builder()
             .with(baseURL: Server.baseURL)
             .with(path: Endpoint.task)
@@ -35,17 +33,43 @@ class TaskRepository {
                 "shift": "\(shift)",
                 "frequency": frequency.map { $0.rawValue },
                 "order": 0
-                ]))
+            ]))
             .with(addHeaderName: "Authorization", value: "Bearer \(token)")
             .with(addHeaderName: "User-Agent", value: "LoryBlu(iOS)")
             .with(addHeaderName: "Content-Type", value: "application/json")
             .build()
-
+        
         do {
             _ = try await network.request(request: request, returning: ResponseMessage.self)
             return true
         } catch {
             return false
         }
+    }
+    
+    func fetchTasks(token: String, childrenId: Int) async -> [TaskModel] {
+        var tasks: [TaskModel] = []
+        let request = RequestModel.Builder()
+            .with(baseURL: Server.baseURL)
+            .with(path: "\(Endpoint.task)?childrenId=\(childrenId)&frequency=sun,mon,tue,wed,thu,fri,sat")
+            .with(method: .get)
+            .with(addHeaderName: "Authorization", value: "Bearer \(token)")
+            .build()
+        
+        do {
+            let result = try await network.request(request: request, returning: ResponseData<NetworkDataModel>.self)
+            tasks = getTasks(type: LBStrings.Locbook.titleStudy,tasks: result.data?.study ?? [])
+            tasks += getTasks(type: LBStrings.Locbook.titleRotine, tasks : result.data?.routine ?? [])
+            
+            return tasks
+        } catch {
+            return tasks
+        }
+    }
+}
+
+extension TaskRepository {
+    func getTasks(type: String, tasks:[TasksNetworkModel]) -> [TaskModel] {
+        return tasks.toTasksModel(type: type)
     }
 }
