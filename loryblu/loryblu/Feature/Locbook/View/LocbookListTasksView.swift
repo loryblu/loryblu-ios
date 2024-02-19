d2e4043a61be51efc52178b708b53feca3707543
Rodrigo <rodrigocavalcante.pro@gmail.com>
2024-02-18 22:45:22 -0300

    [LOR-225] - Display tasks

--- loryblu/loryblu/Feature/Locbook/View/LocbookListTasksView.swift
+++ loryblu/loryblu/Feature/Locbook/View/LocbookListTasksView.swift
@@ -9,21 +9,15 @@ struct LocbookListTasksView: View {
         ShiftItem(name: LBStrings.FrequencyRotine.afternoon, icon: LBIcon.eviningSmall.rawValue, backgroundColor: LBColor.buttonBackgroundMedium,letterColor: .white, isSelected: true),
         ShiftItem(name: LBStrings.FrequencyRotine.night, icon: LBIcon.moonSmall.rawValue,backgroundColor: LBColor.buttonBackgroundDark, letterColor: .white,isSelected: false)]
 
-    // TODO: Implementar chamada da model e filtro
-    let listTasksModelEXEMPLO: [CardTaskRegistered] = [
-        CardTaskRegistered(nameAction: LBStrings.Locbook.titleRotine, imageTask: LBIcon.bathTime.rawValue, nameTask: LBStrings.NameImage.bathTime, backgroundCard: LBColor.buttonBackgroundLight, isSecurity: .constant(true)),
-        CardTaskRegistered(nameAction: LBStrings.Locbook.titleStudy, imageTask: LBIcon.school.rawValue, nameTask: LBStrings.NameImage.bathTime, backgroundCard: LBColor.buttonBackgroundMedium, isSecurity: .constant(false)),
-        CardTaskRegistered(nameAction: LBStrings.Locbook.titleRotine, imageTask: LBIcon.bathTime.rawValue, nameTask: LBStrings.NameImage.bathTime, backgroundCard: LBColor.buttonGenderEnable, isSecurity: .constant(true)),
-        CardTaskRegistered(nameAction: LBStrings.Locbook.titleRotine, imageTask: LBIcon.bathTime.rawValue, nameTask: LBStrings.NameImage.bathTime, backgroundCard: LBColor.buttonBackgroundLight, isSecurity: .constant(true))
-    ]
-
     // TODO: Implementar navegação
     struct Props {
-        var onClose: ClosureType.VoidVoid?
+        let onNewTask: ClosureType.VoidVoid?
     }
 
     var props: Props
 
+    @State var viewmodel: TasksViewModel  = TasksViewModel()
+    
     var body: some View {
         VStack() {
             VStack(spacing: 16) {
@@ -40,36 +34,65 @@ struct LocbookListTasksView: View {
                 LBShiftItemsComponent(shifts: shiftsEXEMPLO)
             }
             .padding(.init(top: 16, leading: 24, bottom: 0, trailing: 24))
+            
+            HStack(alignment: .center){
+                             Toggle(isOn: $securityIsOn) {
+                                 Image(systemName: securityIsOn ? "lock" : "lock.open")
+                             }
+                             .frame(width: 52, height: 32)
+                             .toggleStyle(
+                                 SwitchToggleStyle(
+                                     tint: securityIsOn ? LBColor.buttonBackgroundDark : LBColor.placeholder)
+                             ).padding(.trailing, 6)
+            }.frame(maxWidth: .infinity,alignment: .trailing).padding(.init(top: 8, leading: 0, bottom: 0, trailing: 32))
 
-            listTasks
+            ZStack {
+                ListTasksView(viewmodel: viewmodel,securityIsOn: $securityIsOn).frame(maxHeight:.infinity)
+                    .onAppear { Task {
+                         await viewmodel.fetchTasks()
+                    }
+                }.padding(.init(top: 0, leading: 8, bottom: 0, trailing: 8))
+
+                
+                HStack {
+                    Spacer()
+                    LBfloatingButton(
+                        text: LBStrings.Locbook.button,
+                        icon: LBIcon.plus
+                    ) {
+                        //self.props.onNewTask?()
+                    }
+                }.frame(maxHeight:.infinity,alignment: .bottomTrailing).padding(.init(top: 0, leading: 0, bottom: 33, trailing: 16))
+            }
+            
 
         }
-        .locbookToolbar(title: LBStrings.Locbook.title, onClose: { props.onClose?() })
+        .locbookToolbar(title: LBStrings.Locbook.title, showCloseButton: false)
         .backgroundStyle(LBColor.background)
 
     }
+}
 
-    var listTasks: some View {
-        List() {
-            HStack(alignment: .center){
-                Spacer()
-                Toggle(isOn: $securityIsOn) {
-                    Image(systemName: securityIsOn ? "lock" : "lock.open")
+struct ListTasksView : View {
+    
+    @ObservedObject var viewmodel:TasksViewModel
+    @Binding var securityIsOn: Bool
+
+    var body: some View {
+
+        List {
+            
+            if(!viewmodel.tasks.isEmpty) {
+                ForEach(viewmodel.tasks, id: \.uuid) { model in
+                    CardTaskRegistered(nameAction: model.actionType, imageTask: model.image, nameTask: model.locbookTask.categoryTitle!, backgroundCard: LBColor.buttonBackgroundLight, isSecurity: .constant(true))
                 }
-                .frame(width: 52, height: 32)
-                .toggleStyle(
-                    SwitchToggleStyle(
-                        tint: securityIsOn ? LBColor.buttonBackgroundDark : LBColor.placeholder)
-                )
+                .listRowSeparator(.hidden)
             }
-
-            ForEach(0..<listTasksModelEXEMPLO.count) { index in
-                listTasksModelEXEMPLO[index]
-            }
-            .listRowSeparator(.hidden)
         }
+        .listStyle(.plain)
         .scrollContentBackground(.hidden)
         .scrollIndicators(ScrollIndicatorVisibility.hidden)
+        
     }
 }
 
@@ -84,5 +107,5 @@ extension LocbookListTasksView.Props: Hashable {
 }
 
 #Preview {
-    LocbookListTasksView(props: .init(onClose: {}))
+    LocbookListTasksView(props: .init(onNewTask: {}))
 }
