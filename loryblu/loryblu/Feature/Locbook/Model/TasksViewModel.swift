77a5c3dc78f0f646c74c73a49291a5865a05e2f6
Rodrigo <rodrigoeduardosilv@gmail.com>
2024-02-22 17:24:52 -0300

    add filter in model

--- loryblu/loryblu/Feature/Locbook/Model/TasksViewModel.swift
+++ loryblu/loryblu/Feature/Locbook/Model/TasksViewModel.swift
@@ -4,13 +4,28 @@ import Factory
 class TasksViewModel: ObservableObject {
      @Injected(\.appData) var appData
      
-     private var repository = Container.shared.taskRepository()
+    private var repository = Container.shared.taskRepository()
 
-     @Published var tasks: [TaskModel] = []
+    @Published var tasks: [TaskModel] = []
+    private var listTask: [TaskModel] = []
 
-     @MainActor
-     func fetchTasks() async {
-         tasks = await repository
+    @MainActor
+    func fetchTasks() async {
+        listTask = await repository
              .fetchTasks(token: appData.token,childrenId: appData.childrenId)
-     }
+        tasks = listTask
+    }
+
+    func filterWeekDay(weekDays: [LocbookTask.Frequency]) {
+           var taskFiltered: [TaskModel] = []
+
+           if weekDays == [] {
+               self.tasks = listTask
+           } else {
+               taskFiltered = listTask.filter({ task in
+                   Set(weekDays).intersection(Set(task.locbookTask.frequency ?? [])).isEmpty == false
+               })
+               self.tasks = taskFiltered
+           }
+       }
  }
--- loryblu/loryblu/Feature/Locbook/View/LBDaysButtonStyle.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LBDaysButtonStyle.swift
-//  LoryBlu
-//
-//  Created by Rodrigo Eduardo Silva on 22/02/24.
-//
-
-import Foundation
