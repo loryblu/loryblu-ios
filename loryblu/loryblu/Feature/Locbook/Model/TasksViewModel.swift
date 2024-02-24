e7d4f41f2bece4944e1e3a60db10fffd5dfe20c4
Rodrigo <rodrigocavalcante.pro@gmail.com>
2024-02-24 02:29:28 -0300

    [LOR-229] - get tasks and selectedDayDefault starts from sun, switches to next day if empty.

--- loryblu/loryblu/Feature/Locbook/Model/TasksViewModel.swift
+++ loryblu/loryblu/Feature/Locbook/Model/TasksViewModel.swift
@@ -19,13 +19,32 @@ class TasksViewModel: ObservableObject {
     func filterWeekDay(weekDays: [LocbookTask.Frequency]) {
            var taskFiltered: [TaskModel] = []
 
-           if weekDays == [] {
-               self.tasks = listTask
-           } else {
-               taskFiltered = listTask.filter({ task in
-                   Set(weekDays).intersection(Set(task.locbookTask.frequency ?? [])).isEmpty == false
-               })
-               self.tasks = taskFiltered
-           }
-       }
- }
+extension TasksViewModel {
+    func pairDefaultDayNTasks(tasks: [TaskModel]) async ->
+    (defaultDay: LocbookTask.Frequency, tasksFiltered: [TaskModel]) {
+        var tasksFilted: [TaskModel]?
+        var dayDefault: LocbookTask.Frequency?
+        let week = [LocbookTask.Frequency.sun,
+                    LocbookTask.Frequency.mon,
+                    LocbookTask.Frequency.tue,
+                    LocbookTask.Frequency.wed,
+                    LocbookTask.Frequency.thu,
+                    LocbookTask.Frequency.fri,
+                    LocbookTask.Frequency.sat]
+        var count = 0
+        while(tasksFilted == nil) {
+            let actualTasks = tasks.filter({ task in
+                Set([week[count]]).intersection(Set(task.locbookTask.frequency ?? [])).isEmpty == false
+            })
+            if(!actualTasks.isEmpty) {
+                tasksFilted = tasks
+            }
+            if(count == week.count && tasksFilted == nil) {
+                tasksFilted = []
+            }
+            dayDefault = tasksFilted != nil ? week[count] : LocbookTask.Frequency.sun
+            count += 1
+        }
+        return (defaultDay: dayDefault!, tasksFiltered: tasksFilted ?? [])
+    }
+}
