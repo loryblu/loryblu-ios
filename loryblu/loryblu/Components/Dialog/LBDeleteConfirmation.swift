2c5677c74512d6dd10110b8f566dfc729dbbe3b3
Rodrigo <rodrigoeduardosilv@gmail.com>
2024-06-05 22:28:44 -0300

    add LBString Dialog

--- loryblu/loryblu/Components/Dialog/LBDeleteConfirmation.swift
+++ loryblu/loryblu/Components/Dialog/LBDeleteConfirmation.swift
@@ -1,50 +1,61 @@
 import SwiftUI
 
 struct LBDeleteConfirmation: View {
+    @Environment(\.dismiss) var dismiss
     let onClose: () -> Void
     let nameTask: String
     let taskDay: String
     @State private var offset: CGFloat = 1000
 
     var body: some View {
-        VStack {
-            Text("Tarefa deletada:")
-                .font(.title3)
-                .bold()
+        ZStack {
+            Color(.black)
+            .opacity(0.5)
+                .onTapGesture {
+                    dismiss()
+                }
+            VStack {
+                Text(LBStrings.Dialog.taskDelete)
+                    .font(LBFont.button)
+                    .bold()
 
-            Text(nameTask)
-                .font(.title3)
-                .foregroundColor(LBColor.titlePrimary)
-                .bold()
-                .padding(.bottom, 20)
+                Text(nameTask)
+                    .font(LBFont.button)
+                    .foregroundColor(LBColor.titlePrimary)
+                    .bold()
+                    .padding(.bottom, 20)
 
-            Text("Deletada com sucesso de")
-            Text(taskDay + "!")
-                .bold()
-            LBButton(title: "Fechar") {
-                onClose()
+                Text(LBStrings.Dialog.sucessDelete)
+                    .font(LBFont.bodySmall)
+
+                Text(taskDay + "!")
+                    .bold()
+                    .padding(.bottom, 20)
+
+                LBButton(title: "Fechar") {
+                    dismiss()
+                }
+                .padding(.trailing, 15)
+                .padding(.leading, 15)
             }
-            .padding(10)
-        }
-        .padding()
-        .background(LBColor.textfield)
-        .clipShape(RoundedRectangle(cornerRadius: 24))
-        .frame(maxWidth: .infinity)
-        .padding()
-        .offset(x:0 ,y: offset)
-        .onAppear(perform: {
-            withAnimation(.bouncy) {
-                offset = 0
-            }
+            .padding(24)
+            .background(LBColor.textfield)
+            .clipShape(RoundedRectangle(cornerRadius: 24))
+            .frame(maxWidth: .infinity)
+            .padding(24)
+            .offset(x: 0, y: offset)
+            .onAppear(perform: {
+                withAnimation(.bouncy) {
+                    offset = 0
+                }
         })
-
-
-
+        }
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
     LBDeleteConfirmation(onClose: {
 
-    }, nameTask: "Escovar os dente", taskDay: "segunda-feira")
+    }, nameTask: "Escovar os dentes", taskDay: "segunda-feira")
 }
--- loryblu/loryblu/Style/LBStrings.swift
+++ loryblu/loryblu/Style/LBStrings.swift
@@ -207,3 +207,10 @@ extension LBStrings {
         static let summaryFinishedEditTaskMessage = String(localized: "summary_finished_edit_task_message")
     }
 }
+
+extension LBStrings {
+    enum Dialog {
+        static let taskDelete = String(localized: "dialog_delete_task")
+        static let sucessDelete = String(localized: "dialog_success_delete")
+    }
+}
