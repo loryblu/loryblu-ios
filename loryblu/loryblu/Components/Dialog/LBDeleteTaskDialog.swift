39477b01f55bb07a32cfa78a829e1db001a0d7ec
Rodrigo <72306040+rodrigoliveirac@users.noreply.github.com>
2024-06-24 18:58:26 -0300

    [LOR - 314] - Criar Layout Model de Exclusão (#98)
    * [LOR-314] - Creating LBRadioButton Content
    
    * [LOR-314] - Creating LBCustomDialog Content
    
    * [LOR-314] - supporting error style
    
    * [LOR-314] - Creating LBDeleteTaskDialog
    
    * [LOR-314] Putting a background layer behind the dialog
    
    * [LOR-314] Refactoring
    
    * [LOR-314] - Display modal after user click to delete task
    
    * [LOR-314] drop the hardcode
    
    * [LOR-314]-fix name
    
    * [LOR-314] - updating strings

---
+++ loryblu/loryblu/Components/Dialog/LBDeleteTaskDialog.swift
@@ -0,0 +1,69 @@
+import SwiftUI
+
+enum Option {
+  case option1
+  case option2
+}
+
+struct LBDeleteTaskDialog: View {
+    let dayOfWeek: String
+    let taskName: String
+    let onDelete: () -> Void = {}
+    let onCancel: () -> Void
+    @State var selectedOption: Option? = .option2
+
+    var body: some View {
+        LBCustomDialog(dismiss: onCancel) {
+            Text(LBStrings.Dialog.deleteTaskTitle)
+                .font(.title2)
+                .bold()
+            HStack {
+                Text(taskName)
+                    .font(.title2)
+                    .bold()
+                    .foregroundColor(LBColor.titlePrimary)
+                    .multilineTextAlignment(.center)
+                Text(LBStrings.General.questionMark)
+                    .font(.title2)
+                    .bold()
+                    .multilineTextAlignment(.center)
+            }
+            Spacer(minLength: 16)
+            Text(LBStrings.Dialog.warning)
+                .font(.system(size: 12, weight: .regular))
+                .padding(24)
+                .multilineTextAlignment(.center)
+            Spacer(minLength: 16)
+            VStack(alignment: .listRowSeparatorLeading) {
+                LBRadioButton(tag: .option1, selection: $selectedOption) {
+                    Text(LBStrings.Dialog.deleteOnlyForOneday)
+                    Text(dayOfWeek).bold()
+                }
+                LBRadioButton(tag: .option2, selection: $selectedOption) {
+                    Text(LBStrings.Dialog.deleteForAllDays)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+
+            Spacer(minLength: 24)
+
+            HStack {
+                Button {
+                   onCancel()
+                } label: {
+                    Text(LBStrings.General.cancel).foregroundColor(Color.black)
+                }
+                .contentShape(Rectangle())
+                Spacer().frame(maxWidth: .infinity)
+                LBButton(title: LBStrings.Dialog.deleteTitleBtn, style: .error) {
+                    onDelete()
+                }
+            }.padding(16)
+        }
+    }
+
+}
+
+#Preview {
+    LBDeleteTaskDialog(dayOfWeek: LBStrings.DaysOfWeek.monday, taskName: LBStrings.NameImage.tvgame, onCancel: { })
+}
