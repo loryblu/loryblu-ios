87f269114cbdfdd42be3a9c13d6ec8b21f3f992e
Rodrigo <rodrigoeduardosilv@gmail.com>
2024-08-20 22:25:47 -0300

    add LBMenuCellPerson

---
+++ loryblu/loryblu/Components/Dialog/LBMenuCellPerson.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct LBMenuCell: View {
+    let onClick: () -> Void
+    let 
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LBMenuCell()
+}
