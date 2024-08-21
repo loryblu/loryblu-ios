892ea5488abcb6221abd6a0099c2565011ff9c9a
Rodrigo <rodrigoeduardosilv@gmail.com>
2024-08-20 22:46:39 -0300

    add button action

--- loryblu/loryblu/Components/Dialog/LBMenuCellPerson.swift
+++ loryblu/loryblu/Components/Dialog/LBMenuCellPerson.swift
@@ -1,14 +1,43 @@
 import SwiftUI
 
-struct LBMenuCell: View {
+struct LBMenuCellPerson: View {
     let onClick: () -> Void
-    let 
+    let namePerson: String
+    let image: Image
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ZStack {
+                Circle()
+                    .fill(.white)
+                    .frame(width: 40, height: 40)
+                    .padding(6)
+                image
+            }
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Nome da criança")
+                    .font(LBFont.caption1)
+                Text(namePerson)
+                    .font(LBFont.bodyLarge)
+            }
+            Spacer()
+            Button {
+                onClick()
+            } label: {
+                
+            }
+
+
+        }
+        .frame(width: 327,height: 80)
+        .background(LBColor.textfield)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+
     }
 }
 
 #Preview {
-    LBMenuCell()
+    LBMenuCellPerson(onClick: {
+
+    }, namePerson: "Zeider", image: LBIcon.cake.image)
 }
