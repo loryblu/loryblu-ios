afb4f06e251f74f81a63023c4f955ba17e1599a7
Rodrigo <rodrigoeduardosilv@gmail.com>
2024-08-21 22:06:13 -0300

    add button e stroke line

--- loryblu/loryblu/Components/Dialog/LBMenuCellPerson.swift
+++ loryblu/loryblu/Components/Dialog/LBMenuCellPerson.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct LBMenuCellPerson: View {
     let onClick: () -> Void
+    let functionPerson: String
     let namePerson: String
     let image: Image
 
@@ -13,9 +14,10 @@ struct LBMenuCellPerson: View {
                     .frame(width: 40, height: 40)
                     .padding(6)
                 image
+                    .frame(width: 20, height: 20)
             }
             VStack(alignment: .leading, spacing: 2) {
-                Text("Nome da criança")
+                Text(functionPerson)
                     .font(LBFont.caption1)
                 Text(namePerson)
                     .font(LBFont.bodyLarge)
@@ -24,20 +26,24 @@ struct LBMenuCellPerson: View {
             Button {
                 onClick()
             } label: {
-                
+                LBIcon.arrowRight.image
+                    .frame(width: 6, height: 12)
+                    .padding()
             }
 
-
         }
-        .frame(width: 327,height: 80)
+        .frame(width: 327, height: 80)
         .background(LBColor.textfield)
         .clipShape(RoundedRectangle(cornerRadius: 12))
-
+        .overlay {
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(LBColor.placeholder)
+        }
     }
 }
 
 #Preview {
     LBMenuCellPerson(onClick: {
 
-    }, namePerson: "Zeider", image: LBIcon.cake.image)
+    }, functionPerson: LBStrings.Menu.childName, namePerson: "Roberta Dias", image: LBIcon.childTree.image)
 }
