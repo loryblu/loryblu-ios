58e55eff7ce96579d5fbe499ac92e7921d3041a3
Rodrigo-Eduardo-Silva <97069367+Rodrigo-Eduardo-Silva@users.noreply.github.com>
2024-09-03 20:36:25 -0300

    [LOR-251] Bug Fix - Tratamento de erro no Login (#106)
    * add LBStrings
    
    * add catch erro
    
    * add load view
    
    * delete error 500
    
    * reused updateStateLogin

--- loryblu/loryblu/Feature/Login/LoginModel.swift
+++ loryblu/loryblu/Feature/Login/LoginModel.swift
@@ -4,21 +4,35 @@ import Foundation
 class LoginModel: ObservableObject {
     enum LoginStatus {
         case none
+        case processing
         case success
         case fail
     }
+
+    @Published var loginStatus: LoginStatus = .none
+    @Published var networkError: String = ""
 
     private var repository = Container.shared.autenticationRepository()
 
     @MainActor
     func authenticate(email: String, password: String) {
+        loginStatus = .processing
         Task {
             do {
                 let result = try await repository.login(email: email, password: password)
                 Container.shared.appData().setLoginStatusLogged(user: result)
-                print(result)
-            } catch {
-                print("Deu errado!")
+                loginStatus = .success
+             } catch {
+                let networkError = error as NSError
+                 switch networkError.code {
+                 case 401:
+                     self.networkError = LBStrings.Login.userNotFound
+                 case 422:
+                     self.networkError = LBStrings.Login.userNotFound
+                 default:
+                     self.networkError = LBStrings.Login.serverError
+                 }
+                 loginStatus = .fail
             }
         }
     }
