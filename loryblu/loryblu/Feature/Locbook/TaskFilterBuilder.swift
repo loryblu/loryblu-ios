4233abc4f6e64008c832641e0186f0220211a7e6
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-09-13 20:00:11 -0300

    [LOR-316] Create TaskFilter Builder

--- loryblu/loryblu/Feature/Locbook/TaskFilterBuilder.swift
+++ loryblu/loryblu/Feature/Locbook/TaskFilterBuilder.swift
@@ -1,8 +1,78 @@
-//
-//  TaskFilterBuilder.swift
-//  LoryBlu
-//
-//  Created by Rodrigo Cavalcante on 13/09/24.
-//
-
 import Foundation
+
+class TaskFilterBuilder {
+
+    private var taskFilter: TaskFilter = TaskFilter()
+
+    func setupFilter(tasks: [Int: TaskModel]) {
+        let day = pairDefaultDayNTasks(tasks: Array(tasks.values))
+        taskFilter.day = day.defaultDay
+
+        let shift = pairDefaultShiftNTasks(tasks: day.tasksFiltered)
+        taskFilter.shift = shift.defaultShift
+        
+        taskFilter.tasks = shift.tasksFiltered
+        
+        taskFilter.filterByWeekDay(weekDays: [taskFilter.day ?? LocbookTask.Frequency.sun], allTasks: tasks)
+    }
+        
+    func build() -> TaskFilter {
+        return self.taskFilter
+    }
+}
+
+extension TaskFilterBuilder {
+    func pairDefaultDayNTasks(tasks: [TaskModel])  ->
+    (defaultDay: LocbookTask.Frequency, tasksFiltered: [TaskModel]) {
+        var tasksFiltered: [TaskModel]?
+        var dayDefault: LocbookTask.Frequency = LocbookTask.Frequency.sun
+        let week = [LocbookTask.Frequency.sun,
+                    LocbookTask.Frequency.mon,
+                    LocbookTask.Frequency.tue,
+                    LocbookTask.Frequency.wed,
+                    LocbookTask.Frequency.thu,
+                    LocbookTask.Frequency.fri,
+                    LocbookTask.Frequency.sat]
+        var count = 0
+
+        while tasksFiltered == nil {
+            let actualTasks = tasks.filter({ task in
+                Set([week[count]]).intersection(Set(task.locbookTask.frequency ?? [])).isEmpty == false
+            })
+            if !actualTasks.isEmpty {
+                tasksFiltered = actualTasks
+                dayDefault = tasksFiltered != nil ? week[count] : LocbookTask.Frequency.sun
+            }
+            if count == week.count && tasksFiltered == nil {
+                tasksFiltered = []
+            }
+            count += 1
+        }
+        return (defaultDay: dayDefault, tasksFiltered: tasksFiltered ?? [])
+    }
+
+    func pairDefaultShiftNTasks(tasks: [TaskModel])  -> (defaultShift: LocbookTask.Shift, tasksFiltered: [TaskModel]) {
+        var tasksFiltered: [TaskModel]?
+        var shiftDefault: LocbookTask.Shift = LocbookTask.Shift.morning
+        let shiftsItems = [
+            LocbookTask.Shift.morning,
+            LocbookTask.Shift.afternoon,
+            LocbookTask.Shift.night]
+        var count = 0
+
+        while tasksFiltered == nil {
+            let actualTasks = tasks.filter({ task in
+                task.locbookTask.shift == shiftsItems[count]
+            })
+            if !actualTasks.isEmpty {
+                tasksFiltered = actualTasks
+                shiftDefault = tasksFiltered != nil ? shiftsItems[count] : shiftDefault
+            }
+            if count == shiftsItems.count && tasksFiltered == nil {
+                tasksFiltered = []
+            }
+            count += 1
+        }
+        return (defaultShift: shiftDefault, tasksFiltered: tasksFiltered ?? [])
+    }
+}
