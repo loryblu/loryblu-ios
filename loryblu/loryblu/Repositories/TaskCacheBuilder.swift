c40dbf4ad19d745f161bac0127e4f84741af183d
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-09-13 19:57:43 -0300

    [LOR-316] - Create cache builder

--- loryblu/loryblu/Repositories/TaskCacheBuilder.swift
+++ loryblu/loryblu/Repositories/TaskCacheBuilder.swift
@@ -1,8 +1,22 @@
-//
-//  TaskCacheBuilder.swift
-//  LoryBlu
-//
-//  Created by Rodrigo Cavalcante on 13/09/24.
-//
-
 import Foundation
+
+class TaskCacheBuilder {
+
+    private var tasks: [Int: TaskModel] = [:]
+
+    func addStudyList(studyTasks: [TaskModel]) {
+        studyTasks.forEach { task in
+            tasks[task.locbookTask.id!] = task
+        }
+    }
+
+    func addRoutineList(routineTasks: [TaskModel]) {
+        routineTasks.forEach { task in
+            tasks[task.locbookTask.id!] = task
+        }
+    }
+
+    func build() -> [Int: TaskModel] {
+        return self.tasks
+    }
+}
