3c167eb9e3092b813626b52bffd9dc468fea58af
Suelen Cristina Vaz <100352837+sucriss2@users.noreply.github.com>
2024-10-15 22:01:19 -0300

    Revert "[BUGFIX][LOR-306] - Constraints genre button and closeButton DoneView" (#113)
    Revert "[BUGFIX][LOR-306] - Constraints genre button and closeButton DoneView…"
    
    This reverts commit 246b8e87fbcc9e22422657ff4c608d03c78c03b6.

--- loryblu/loryblu/Components/Base/LBTextField/LBDatePicker.swift
+++ loryblu/loryblu/Components/Base/LBTextField/LBDatePicker.swift
@@ -1,53 +1,44 @@
 import SwiftUI
 
 struct LBDatePicker: View {
-    @State var selectedDate = Date()
     let onConfirm: (Date) -> Void
     let onCancel: () -> Void
-
+    @State var selectedDate = Date()
     var dateClosedRange: ClosedRange<Date> {
-        let min = Calendar.current.date(
-            byAdding: .year,
-            value: -10,
-            to: Date()) ?? Date.now
-        let max = Calendar.current.date(
-            byAdding: .year,
-            value: 0,
-            to: Date()) ?? Date.now
+        let min = Calendar.current.date(byAdding: .year,
+                                        value: -10,
+                                        to: Date()) ?? Date.now
+        let max = Calendar.current.date(byAdding: .year,
+                                        value: 0,
+                                        to: Date()) ?? Date.now
         return min...max
     }
-
     var body: some View {
-        GeometryReader { _ in
-            ZStack(alignment: .center ) {
-                Color.gray.opacity(0.2)
-                    .ignoresSafeArea()
-                    .overlay {
-                        VStack {
-                            DatePicker(
-                                LBStrings.Register.birthDay,
-                                selection: $selectedDate,
-                                in: dateClosedRange,
-                                displayedComponents: .date
-                            )
-                            .datePickerStyle(.graphical)
-                            .padding(20)
-                            .background(Color.white.cornerRadius(20))
-                            .clipped()
-                            .environment(\.locale, Locale.init(identifier: "pt"))
-                            .foregroundColor(LBColor.background)
-                            
-                            HStack {
-                                Button(LBStrings.General.confirm) {
-                                    onConfirm(selectedDate)
-                                }
-                                Button(LBStrings.General.cancel) {
-                                    onCancel()
-                                }
-                            }.padding(.vertical, -35)
-                        }
-                        .padding(20)
-                    }
+
+        ZStack(alignment: .center ) {
+            Color.gray.opacity(0.2)
+                 .ignoresSafeArea()
+                 .overlay {
+                     VStack {
+                         DatePicker(LBStrings.Register.birthDay, selection: $selectedDate,
+                                    in: dateClosedRange, displayedComponents: .date)
+                             .datePickerStyle(.graphical)
+                             .padding(20)
+                             .background(Color.white.cornerRadius(20))
+                             .clipped()
+                             .environment(\.locale, Locale.init(identifier: "pt"))
+                             .foregroundColor(LBColor.background)
+                         
+                         HStack {
+                             Button(LBStrings.General.confirm) {
+                                 onConfirm(selectedDate)
+                             }
+                             Button(LBStrings.General.cancel) {
+                                 onCancel()
+                             }
+                         }.padding(.vertical,-35)
+                     }
+                     .padding(20)
             }
         }
     }
@@ -55,7 +46,7 @@ struct LBDatePicker: View {
 
 struct LBDatePicker_Previews: PreviewProvider {
     static var previews: some View {
-        LBDatePicker { _ in
+        LBDatePicker { date in
 
         } onCancel: {
 
