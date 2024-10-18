680adf2656b76ca85b9fb8d3f187af5ecc935423
Rodrigo <72306040+rodrigoliveirac@users.noreply.github.com>
2024-10-18 17:30:27 -0300

    LOR 347 - User Logout (#114)
    * LOR-347 - User Logout
    
    * LOR-347  - drop unnecessary line
    
    * LOR-347 - Indenting
    
    * LOR 347 - solve lint error

---
+++ loryblu/loryblu/Feature/Home/MenuViewModel.swift
@@ -0,0 +1,17 @@
+import Foundation
+import Factory
+
+class MenuViewModel: ObservableObject {
+    private let appData = Container.shared.appData()
+    @Published var openExitConfirmationDialog: Bool = false
+    func openExitConfirmation() {
+        openExitConfirmationDialog = !openExitConfirmationDialog
+    }
+    func logout() {
+        appData.logout()
+        openExitConfirmationDialog = !openExitConfirmationDialog
+    }
+    func giveUpToLogout() {
+        openExitConfirmationDialog = !openExitConfirmationDialog
+    }
+}
--- loryblu/loryblu/Helpers/Handlers/AppData.swift
+++ loryblu/loryblu/Helpers/Handlers/AppData.swift
@@ -9,23 +9,25 @@ class AppData: ObservableObject {
     @Published var token: String = ""
     @Published var childrenId: Int = 0
     @Published var userData: UserAuth?
-    var forceReloadListView = false
 
-    var loginStatus: LoginStatus {
-            guard userData != nil else {
-                return .notLogged
-            }
-            return .logged
-        }
+    @Published var loginStatus: LoginStatus = .notLogged
 
     func setLoginStatusLogged(user: UserAuth) {
         Task { @MainActor in
             userData = user
+            if userData != nil {
+                loginStatus = .logged
+            }
             token = user.data.accessToken
 
             if let id = user.data.user.childrens.first?.id {
                 childrenId = id
             }
         }
+    }
+    
+    func logout() {
+        userData = nil
+        loginStatus = .notLogged
     }
 }
