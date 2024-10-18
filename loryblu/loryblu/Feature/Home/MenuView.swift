a0f49c52ec031e0c48ee34f3f55f740133e274e9
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-10-18 17:21:38 -0300

    LOR-347 - Indenting

--- loryblu/loryblu/Feature/Home/MenuView.swift
+++ loryblu/loryblu/Feature/Home/MenuView.swift
@@ -4,7 +4,7 @@ struct MenuView: View {
     @Environment(\.dismiss) var dismiss
     @State var user: User?
     @StateObject var menuViewModel: MenuViewModel = MenuViewModel()
-
+    
     var body: some View {
         ZStack {
             VStack {
@@ -20,7 +20,7 @@ struct MenuView: View {
                     }
                 }
                 .padding()
-
+                
                 VStack(spacing: 15) {
                     HStack {
                         Text(LBStrings.Menu.profile)
@@ -29,18 +29,18 @@ struct MenuView: View {
                         Spacer()
                     }
                     LBMenuCellPerson(
-                                    onClick: {},
-                                    description: LBStrings.Menu.childName,
-                                    name: user?.childrens.first?.fullname ?? "",
-                                    image: LBIcon.childTree.image,
-                                    style: .person
+                        onClick: {},
+                        description: LBStrings.Menu.childName,
+                        name: user?.childrens.first?.fullname ?? "",
+                        image: LBIcon.childTree.image,
+                        style: .person
                     )
                     LBMenuCellPerson(
-                                    onClick: {},
-                                    description: LBStrings.Menu.parentsName,
-                                    name: user?.parentName ?? " ",
-                                    image: LBIcon.parentsTree.image,
-                                    style: .person
+                        onClick: {},
+                        description: LBStrings.Menu.parentsName,
+                        name: user?.parentName ?? " ",
+                        image: LBIcon.parentsTree.image,
+                        style: .person
                     )
                     HStack {
                         Text(LBStrings.Menu.configuration)
@@ -48,46 +48,45 @@ struct MenuView: View {
                             .foregroundStyle(LBColor.loryGray)
                         Spacer()
                     }
-
+                    
                     LBMenuCellPerson(
-                                    onClick: {},
-                                    description: LBStrings.Menu.accessControl,
-                                    name: LBStrings.Menu.security,
-                                    image: LBIcon.shield.image,
-                                    style: .configuration
+                        onClick: {},
+                        description: LBStrings.Menu.accessControl,
+                        name: LBStrings.Menu.security,
+                        image: LBIcon.shield.image,
+                        style: .configuration
                     )
                     LBMenuCellPerson(
-                                    onClick: {},
-                                    description: "",
-                                    name: LBStrings.Menu.faq,
-                                    image: LBIcon.questionMark.image,
-                                    style: .configuration
+                        onClick: {},
+                        description: "",
+                        name: LBStrings.Menu.faq,
+                        image: LBIcon.questionMark.image,
+                        style: .configuration
                     )
                     LBMenuCellPerson(
-                                    onClick: {},
-                                    description: "",
-                                    name: LBStrings.Menu.privacyTerms,
-                                    image: LBIcon.privacyTerms.image,
-                                    style: .configuration
+                        onClick: {},
+                        description: "",
+                        name: LBStrings.Menu.privacyTerms,
+                        image: LBIcon.privacyTerms.image,
+                        style: .configuration
                     )
                     LBMenuCellPerson(
-                                    onClick: {
-                                        menuViewModel.openExitConfirmation()
-                                    },
-                                    description: "",
-                                    name: LBStrings.Menu.exit,
-                                    image: LBIcon.exit.image,
-                                    style: .exit
+                        onClick: {
+                            menuViewModel.openExitConfirmation()
+                        },
+                        description: "",
+                        name: LBStrings.Menu.exit,
+                        image: LBIcon.exit.image,
+                        style: .exit
                     )
                     .padding(.top, 16)
-
+                    
                 }
                 .padding(20)
-
+                
             }
             .frame(maxWidth: .infinity)
             .padding(.init(top: 50, leading: 0, bottom: 0, trailing: 0))
-            
             if menuViewModel.openExitConfirmationDialog {
                 LBExitAppConfirmation {
                     menuViewModel.logout()
@@ -101,9 +100,9 @@ struct MenuView: View {
 
 #Preview {
     MenuView(user: User(
-                        parentName: "Rodrigo",
-                        childrens: [Child(id: 01,
-                                          fullname: "Zeider Silva",
-                                          gender: "male",
-                                          birthdate: "27/01/2020")]))
+        parentName: "Rodrigo",
+        childrens: [Child(id: 01,
+                          fullname: "Zeider Silva",
+                          gender: "male",
+                          birthdate: "27/01/2020")]))
 }
