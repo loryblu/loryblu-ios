f68c10c5d76f563050210296bb9d51a607f42145
Suelen Cristina Vaz <100352837+sucriss2@users.noreply.github.com>
2024-11-25 09:18:35 -0300

    [BUGFIX][LOR-307] Rules size password (#117)
    * Changed rules and conditions
    
    * delete line

--- loryblu/loryblu/Helpers/Validation/ValidateRules.swift
+++ loryblu/loryblu/Helpers/Validation/ValidateRules.swift
@@ -4,7 +4,7 @@ import SwiftUI
 enum ValidateRules {
 
     static func validateName(_ name: String) -> Bool {
-        let nameRegex = "^[a-zA-ZÀ-ÖØ-öø-ÿ]{3,}(\\s[a-zA-ZÀ-ÖØ-öø-ÿ]+){0,1}(\\s[a-zA-ZÀ-ÖØ-öø-ÿ]+){0,1}$"
+        let nameRegex = "^[a-zA-ZÀ-ÖØ-öø-ÿ]{3,}(\\s[a-zA-ZÀ-ÖØ-öø-ÿ]+){1,}(\\s[a-zA-ZÀ-ÖØ-öø-ÿ]+){0,1}$"
         let namePredicate = NSPredicate(format: "SELF MATCHES %@", nameRegex)
         return namePredicate.evaluate(with: name)
     }
@@ -45,4 +45,7 @@ enum ValidateRules {
         return passwordPredicate.evaluate(with: password)
     }
 
+    static func validatePassword(password: String) -> Bool {
+        validateSize(password: password) && validateUppercase(password: password) && validateLowercase(password: password) && validateNumber(password: password) && validateSpecial(password: password)
+    }
 }
