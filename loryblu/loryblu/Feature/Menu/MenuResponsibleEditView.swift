fec8421c2a7261f9535d9c77554c8ee158ae84fe
Rodrigo <rodrigoeduardosilv@gmail.com>
2024-12-04 20:53:40 -0300

    add lb textField

--- loryblu/loryblu/Feature/Menu/MenuResponsibleEditView.swift
+++ loryblu/loryblu/Feature/Menu/MenuResponsibleEditView.swift
@@ -6,21 +6,14 @@ struct MenuResponsibleEditView: View {
     var body: some View {
         VStack {
             HStack {
-                ZStack {
-                    Circle()
-                        .fill(.white)
-                        .frame(width: 96, height: 96)
-                    .padding()
-                    image
-                        .frame(width: 79, height: 79)
-
-                }
+                imageDefault
             }
             .frame(maxWidth: .infinity)
             .frame(height: 148)
             .background(LBColor.textfield)
+            .padding(.bottom, 50)
 
-            VStack(alignment:.leading){
+            VStack(alignment: .leading) {
                 Text("Nome da pessoa responsável")
                     .font(LBFont.buttonSmall)
                 LBTextField(style: .common,
@@ -34,27 +27,59 @@ struct MenuResponsibleEditView: View {
                     .font(LBFont.buttonSmall)
                 LBTextField(style: .common,
                             icon: LBIcon.mail,
-                            title: "" ,
+                            title: "",
                             text: $user.parentName,
                             textFiledState: .disable)
+                HStack {
+                    Image(systemName: "info.circle")
+                        .foregroundColor(.gray)
+                        .frame(width: 15, height: 15)
+                    Text("Náo é possivel editar o e-mail. ")
+                        .font(.caption2) +                    Text("Entre em Contato")
+                        .bold()
+                        .font(.caption2)
+                        .underline() +
+                    Text(" com o nosso suporte para mais informações")
+                        .font(.caption2)
+                }
+                .padding(.top, 10)
+                .padding(.bottom, 20)
 
                 Text("Senha")
                     .font(LBFont.buttonSmall)
                 LBTextField(style: .password,
                             icon: LBIcon.lock,
-                            title: "" ,
+                            title: "",
                             text: $user.parentName,
-                            textFiledState: .disable)
-                .padding(.bottom, 30)
+                            textFiledState: .active)
+                .padding(.bottom, 20)
+
+                HStack {
+                    LBButton(title: "Cancelar") {
+
+                    }
+
+                    LBButton(title: "Salvar") {
+                        
+                    }
+                }
             }
             .padding()
         }
     }
+
+    var imageDefault: some View {
+        ZStack {
+            Circle()
+                .fill(.white)
+                .frame(width: 96, height: 96)
+                .padding()
+            image
+                .frame(width: 79, height: 79)
+        }
+    }
 }
 
-extension MenuResponsibleEditView {
-    
-}
 #Preview {
     MenuResponsibleEditView(image: LBIcon.parentsTree2.image,
                             user: .constant(User(
