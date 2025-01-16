0543a47c9894acdafd20d46a38bdcd32190e218f
Suelen Critina Vaz <sucris12@gmail.com>
2025-01-16 18:46:26 -0300

    Create coordinator and navigationStack for menu

---
+++ loryblu/loryblu/Feature/Menu/MenuNavigationStack.swift
@@ -0,0 +1,83 @@
+//
+//  MenuNavigationStack.swift
+//  LoryBlu
+//
+//  Created by Suh on 16/12/24.
+//
+
+import SwiftUI
+
+struct MenuNavigationStack: View {
+    @EnvironmentObject var appData: AppData
+
+    typealias NavigationCoordinator = MenuCoordinator
+
+    struct Props {
+        let onFinish: ClosureType.VoidVoid?
+        let onDismiss: ClosureType.VoidVoid?
+    }
+
+    @ObservedObject var coordinator: NavigationCoordinator
+    @Environment(\.dismiss) private var dismiss
+
+    private let props: Props
+
+    init(props: Props) {
+        coordinator = .init()
+        self.props = props
+    }
+
+    var body: some View {
+        NavigationStack(path: $coordinator.path) {
+            coordinator.buildView(
+                page: .menu(
+                    .init(
+                        showNextPage: {
+                            pushResponsibleEdit(aappData: appData)
+                    })
+                )
+            )
+            .navigationDestination(
+                for: MenuCoordinator.Destination.Navigation.self
+            ) { destination in
+                coordinator.buildView(page: destination)
+                    .environmentObject(coordinator)
+            }
+        }
+    }
+
+    private func pushResponsibleEdit(aappData: AppData) {
+        print("fdsafdsafds")
+        coordinator.pushResponsibleEditView(
+            props: MenuResponsibleEditView.Props(
+                user: appData.userData!.data.user,
+                image: LBIcon.parentsTree.image,
+                isAvaliable: true
+            )
+        )
+    }
+
+    private func pushChildEdit() {
+        print("----------")
+    }
+
+    private func pushFinishView(message: String) {
+        coordinator.pushFinishScreen(
+            props: .init(
+                message: message,
+                onClose: {
+                    coordinator.popToRoot()
+                }
+            )
+        )
+    }
+}
+
+struct MenuNavigationStack_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuNavigationStack(
+            props: .init(onFinish: nil, onDismiss: nil)
+        )
+            .environmentObject(AppData())
+    }
+}
